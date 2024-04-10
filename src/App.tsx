a732d0d64ca10984bf5a004a0b5abac8183f0241
ritto153 <animal1889@gmail.com>
2024-04-10 23:49:11 +0900

    DragDropContext で複数テーブル囲む

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,59 @@
 import "./App.css";
+import { DragDropContext } from "react-beautiful-dnd";
 import Part from "./components/Part";
 import { useEntries } from "./EntryProvider";
+import { Entry } from "./type/Entry";
+import { DropResult } from "./type/DropResult";
 
 export default function App() {
-  const { entries } = useEntries();
+  const { entries, setEntries } = useEntries();
   const maxPartNum = Math.max(...entries.map((entry) => entry.part_num === null ? 0 : entry.part_num));
+
+  // https://github.com/atlassian/react-beautiful-dnd/blob/013bfceac04ff48548c33cdc468dd2927446fc1b/stories/src/reorder.js#L6
+  const reorderEntry = (
+    list: Entry[],
+    startIndex: number,
+    endIndex: number
+  ): Entry[] => {
+    let result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    result = result.map((entry, i) => ({
+      ...entry,
+      sort: i + 1,
+    }));
+
+    return result;
+  };
+
+  // https://github.com/atlassian/react-beautiful-dnd/blob/013bfceac04ff48548c33cdc468dd2927446fc1b/stories/src/table/with-fixed-columns.jsx#L107
+  const onDragEnd = (result: DropResult) => {
+    // 表の外にドロップされた場合
+    if (!result.destination) return;
+
+    // 同じ場所にドロップされた場合
+    if (result.destination.index === result.source.index) return;
+
+    const reorderedEntries = reorderEntry(
+      entries,
+      result.source.index,
+      result.destination.index
+    );
+    setEntries(reorderedEntries);
+  };
 
   return (
     <div className="App">
-      {/* まだ部に割り振られていないエントリーを表示するテーブル */}
-      <Part part_num={null} />
+      <DragDropContext onDragEnd={onDragEnd}>
+        {/* まだ部に割り振られていないエントリーを表示するテーブル */}
+        <Part part_num={null} />
 
-      {/* 各部のエントリーを表示するテーブル。全エントリーの最大部数まで描画する */}
-      {[...Array(maxPartNum)].map((_, i) => (
-        <Part key={i + 1} part_num={i + 1} />
-      ))}
+        {/* 各部のエントリーを表示するテーブル。全エントリーの最大部数まで描画する */}
+        {[...Array(maxPartNum)].map((_, i) => (
+          <Part key={i + 1} part_num={i + 1} />
+        ))}
+      </DragDropContext>
     </div>
   );
 }
--- src/components/EntryTable.tsx
+++ src/components/EntryTable.tsx
@@ -1,91 +1,53 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 import Table from "react-bootstrap/Table";
 import "bootstrap/dist/css/bootstrap.min.css";
 import EntryTableRow from "./EntryTableRow";
 import { entryAttributesInfo } from "../constants/EntryAttributesInfo";
 import { useEntries } from "../EntryProvider";
-import { Entry } from "../type/Entry";
-import { DropResult } from "../type/DropResult";
 
 type Props = {
   part_num: number | null;
 }
 export default function EntryTable(props: Props) {
   const { part_num } = props;
-  const { entries, setEntries } = useEntries();
+  const { entries } = useEntries();
   const selectedEntries = entries.filter((entry) => entry.part_num === part_num);
 
-  // https://github.com/atlassian/react-beautiful-dnd/blob/013bfceac04ff48548c33cdc468dd2927446fc1b/stories/src/reorder.js#L6
-  const reorderEntry = (
-    list: Entry[],
-    startIndex: number,
-    endIndex: number
-  ): Entry[] => {
-    let result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-
-    result = result.map((entry, i) => ({
-      ...entry,
-      sort: i + 1,
-    }));
-
-    return result;
-  };
-
-  // https://github.com/atlassian/react-beautiful-dnd/blob/013bfceac04ff48548c33cdc468dd2927446fc1b/stories/src/table/with-fixed-columns.jsx#L107
-  const onDragEnd = (result: DropResult) => {
-    // 表の外にドロップされた場合
-    if (!result.destination) return;
-
-    // 同じ場所にドロップされた場合
-    if (result.destination.index === result.source.index) return;
-
-    const reorderedEntries = reorderEntry(
-      selectedEntries,
-      result.source.index,
-      result.destination.index
-    );
-    setEntries(reorderedEntries);
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th></th>
-            {Object.entries(entryAttributesInfo).map(([_, value]) => {
-              if (value["displayInTable"]) return <th>{value["displayName"]}</th>;
-              else return null;
-            })}
-          </tr>
-        </thead>
-        <Droppable droppableId="table">
-          {(droppableProvided) => (
-            <tbody
-              ref={droppableProvided.innerRef}
-              {...droppableProvided.droppableProps}
-            >
-              {selectedEntries.map((entry, i) => (
-                <Draggable
-                  key={entry.id}
-                  draggableId={String(entry.id)}
-                  index={i}
-                >
-                  {(draggableProvided) => (
-                    <EntryTableRow
-                      key={entry.id}
-                      draggableProvided={draggableProvided}
-                      entryId={entry.id}
-                    />
-                  )}
-                </Draggable>
-              ))}
-            </tbody>
-          )}
-        </Droppable>
-      </Table>
-    </DragDropContext>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th></th>
+          {Object.entries(entryAttributesInfo).map(([_, value]) => {
+            if (value["displayInTable"]) return <th>{value["displayName"]}</th>;
+            else return null;
+          })}
+        </tr>
+      </thead>
+      <Droppable droppableId="table">
+        {(droppableProvided) => (
+          <tbody
+            ref={droppableProvided.innerRef}
+            {...droppableProvided.droppableProps}
+          >
+            {selectedEntries.map((entry, i) => (
+              <Draggable
+                key={entry.id}
+                draggableId={String(entry.id)}
+                index={i}
+              >
+                {(draggableProvided) => (
+                  <EntryTableRow
+                    key={entry.id}
+                    draggableProvided={draggableProvided}
+                    entryId={entry.id}
+                  />
+                )}
+              </Draggable>
+            ))}
+          </tbody>
+        )}
+      </Droppable>
+    </Table>
   );
 }
