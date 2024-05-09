573b0ba7f2c4cfbb4ed9c3f7d8d1f2ca2bdcd453
ritto153 <animal1889@gmail.com>
2024-05-10 00:21:56 +0900

    ピクセル数変更

--- src/constants/EntryAttributesInfo.ts
+++ src/constants/EntryAttributesInfo.ts
@@ -1,42 +1,42 @@
 export const entryAttributesInfo = {
   partNum: {
-    columnWidth: 5,
+    columnWidth: 50,
     displayName: "部",
   },
   index: {
-    columnWidth: 5,
+    columnWidth: 50,
     displayName: "番",
   },
   lastName: {
-    columnWidth: 10,
+    columnWidth: 60,
     displayName: "姓",
   },
   firstName: {
-    columnWidth: 10,
+    columnWidth: 60,
     displayName: "名",
   },
   affiliation: {
-    columnWidth: 10,
+    columnWidth: 80,
     displayName: "所属",
   },
   grade: {
-    columnWidth: 5,
+    columnWidth: 50,
     displayName: "学年",
   },
   composer: {
-    columnWidth: 20,
+    columnWidth: 150,
     displayName: "作曲家",
   },
   work: {
-    columnWidth: 25,
+    columnWidth: 300,
     displayName: "曲名",
   },
   time: {
-    columnWidth: 5,
+    columnWidth: 100,
     displayName: "演奏時間",
   },
   memo: {
-    columnWidth: 5,
+    columnWidth: 200,
     displayName: "メモ",
   },
 };
