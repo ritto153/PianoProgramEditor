81c9b25c91282d0f7a7721a5ec26c0c29e9cf0c8
ritto153 <animal1889@gmail.com>
2024-09-23 16:07:01 +0900

    不要なファイルの削除

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import EntryProvider from './EntryProvider';
 import PartProvider from './PartProvider';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,5 +13,3 @@ root.render(
     </EntryProvider>
   </PartProvider>
 );
-
-reportWebVitals();
