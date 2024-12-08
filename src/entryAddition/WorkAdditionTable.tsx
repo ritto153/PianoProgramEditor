a7d37d4a32cc340c14f8f17c79a365f297d76f40
ritto153 <animal1889@gmail.com>
2024-12-08 16:21:15 +0900

    state を使った曲目の表示

--- src/entryAddition/WorkAdditionTable.tsx
+++ src/entryAddition/WorkAdditionTable.tsx
@@ -1,7 +1,11 @@
 import Table from "react-bootstrap/Table";
+import { useEntryToAddContext } from "./EntryToAddProvider";
 import { FaRegPlusSquare, FaRegTrashAlt, FaRegCopy } from "react-icons/fa";
 
 export default function WorkAdditionTable() {
+  const { entry } = useEntryToAddContext();
+  const { works } = entry;
+
   return (
     <Table>
       <thead>
@@ -11,23 +15,27 @@ export default function WorkAdditionTable() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th>
-            <input type="text" />
-          </th>
-          <th>
-            <input type="text" />
-          </th>
-          <th>
-            <FaRegPlusSquare />
-          </th>
-          <th>
-            <FaRegCopy />
-          </th>
-          <th>
-            <FaRegTrashAlt />
-          </th>
-        </tr>
+        {works.map((work, i) => {
+          return (
+            <tr key={i}>
+              <th>
+                <input type="text" value={String(work.composer)} />
+              </th>
+              <th>
+                <input type="text" value={String(work.name)} />
+              </th>
+              <th>
+                <FaRegPlusSquare />
+              </th>
+              <th>
+                <FaRegCopy />
+              </th>
+              <th>
+                <FaRegTrashAlt />
+              </th>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
