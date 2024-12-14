19f2c69a507cc33738a20ebe5486a816a411f18f
ritto153 <animal1889@gmail.com>
2024-12-14 23:25:34 +0900

    table のスタイル変更

--- src/entryAddition/components/WorkAdditionTable.tsx
+++ src/entryAddition/components/WorkAdditionTable.tsx
@@ -1,22 +1,56 @@
-import Table from "react-bootstrap/Table";
+import { useFormContext, useFieldArray } from "react-hook-form";
+import styled from "styled-components";
 import WorkAdditionTableRow from "./WorkAdditionTableRow";
+import { InputtingEntryToAdd } from "../../type/Entry";
+
+const StyledTable = styled.table`
+  width: 100%;
+  text-align: center;
+  table-layout: fixed;
+  border-collapse: collapse;
+  margin-bottom: 10px;
+  td {
+    height: 3em;
+    border: 1px solid #dee2e6;
+    padding-top: 3px;
+    padding-bottom: 3px;
+    letter-spacing: 1px;
+  }
+`;
+
+const ThWithWidth = styled.th<{ $width: number }>`
+  width: ${(props) => props.$width}%;
+`;
 
 export default function WorkAdditionTable() {
-  const workNum = 1; // 変動させる必要あり
+  const { control } = useFormContext<InputtingEntryToAdd>();
+  const { fields, remove, insert } = useFieldArray<InputtingEntryToAdd>({
+    control,
+    name: "works",
+  });
 
   return (
-    <Table>
+    <StyledTable>
       <thead>
         <tr>
-          <th>作曲家</th>
-          <th>曲目</th>
+          <ThWithWidth $width={20}>作曲家</ThWithWidth>
+          <ThWithWidth $width={50}>曲目</ThWithWidth>
+          <ThWithWidth $width={10}>追加</ThWithWidth>
+          <ThWithWidth $width={10}>複製</ThWithWidth>
+          <ThWithWidth $width={10}>削除</ThWithWidth>
         </tr>
       </thead>
       <tbody>
-        {Array.from({ length: workNum }).map((_, i) => (
-          <WorkAdditionTableRow key={i} index={i}/>
+        {fields.map((item, index) => (
+          <WorkAdditionTableRow
+            key={item.id}
+            index={index}
+            count={fields.length}
+            remove={remove}
+            insert={insert}
+          />
         ))}
       </tbody>
-    </Table>
+    </StyledTable>
   );
 }
--- src/entryAddition/components/WorkAdditionTableRow.tsx
+++ src/entryAddition/components/WorkAdditionTableRow.tsx
@@ -1,38 +1,65 @@
+import styled from "styled-components";
 import { FaRegPlusSquare, FaRegTrashAlt, FaRegCopy } from "react-icons/fa";
+import {
+  useFormContext,
+  UseFieldArrayRemove,
+  UseFieldArrayInsert,
+} from "react-hook-form";
 import { InputtingEntryToAdd } from "../../type/Entry";
-import { useFormContext } from "react-hook-form";
 
+const StyledInput = styled.input`
+  width: 90%
+`
 interface Props {
   index: number;
+  count: number;
+  remove: UseFieldArrayRemove;
+  insert: UseFieldArrayInsert<InputtingEntryToAdd>;
 }
 
 export default function WorkAdditionTableRow(props: Props) {
-  const { index } = props;
-  const { register } = useFormContext<InputtingEntryToAdd>();
+  const { index, count, remove, insert } = props;
+  const { register, getValues } = useFormContext<InputtingEntryToAdd>();
+
+  const insertCopiedRow = () => {
+    const work = getValues().works[index];
+
+    insert(index + 1, work);
+  };
 
   return (
     <tr>
-      <th>
-        <input
-          type="text"
-          {...register(`works.${index}.composer`, { required: true })}
-        />
-      </th>
-      <th>
-        <input
-          type="text"
-          {...register(`works.${index}.name`, { required: true })}
-        />
-      </th>
-      <th>
-        <FaRegPlusSquare />
-      </th>
-      <th>
-        <FaRegCopy />
-      </th>
-      <th>
-        <FaRegTrashAlt />
-      </th>
+      <td>
+        {/** 作曲家 */}
+        <StyledInput {...register(`works.${index}.composer`, { required: true })} />
+      </td>
+      <td>
+        {/** 曲名 */}
+        <StyledInput {...register(`works.${index}.name`, { required: true })} />
+      </td>
+      <td>
+        {/** 空の行を下に追加するボタン */}
+        <button
+          type="button"
+          onClick={() => insert(index + 1, { composer: "", name: "" })}
+        >
+          <FaRegPlusSquare />
+        </button>
+      </td>
+      <td>
+        {/** 現在の行をコピーして下に挿入するボタン */}
+        <button type="button" onClick={() => insertCopiedRow()}>
+          <FaRegCopy />
+        </button>
+      </td>
+      <td>
+        {/** 現在の行を削除するボタン */}
+        {count > 1 && (
+          <button type="button" onClick={() => remove(index)}>
+            <FaRegTrashAlt />
+          </button>
+        )}
+      </td>
     </tr>
   );
 }
