6e15758eb807233e851888f4ed2127d0adc5a73d
ritto153 <animal1889@gmail.com>
2025-06-07 18:06:08 +0900

    CSVダウンロードを修正

--- src/csvDownload/CsvDownloadButton.tsx
+++ src/csvDownload/CsvDownloadButton.tsx
@@ -1,8 +1,13 @@
 import { CSVLink } from "react-csv";
-import { Entry } from "../type/Entry";
 import Button from "react-bootstrap/Button";
 import styled from "styled-components";
 import { useGetSavedData } from "../hooks/useGetSavedData";
+import { DivideEntryForRow } from "../partContents/utils/BuildEntriesForTableRow";
+import { BuildEntrySchedules } from "../partHeader/utils/BuildEntrySchedules";
+import { StringifyDate } from "../utils/StringifyDate";
+import { Part, PartMap } from "../type/Part";
+import { EntryMap } from "../type/Entry";
+
 
 const AlignRightDiv = styled.div`
   text-align: center;
@@ -16,21 +21,68 @@ export default function CsvDownloadButton() {
   const partMap = savedDataInUse.partMap;
   const entryMap = savedDataInUse.entryMap;
 
-  const sortedParts = Object.values(partMap).sort(
-    (a, b) => a.partNum - b.partNum
-  );
+  const csvHeader = BuildCsvHeader();
+  const csvData = BuildCsvData(partMap, entryMap);
 
-  const csvData: Entry[] = sortedParts
-  .map((entry) =>
-    Object.values(entry.entryIds).map((entryId) => entryMap[entryId])
-  )
-  .flat();
+  console.log("CSV Data:", csvData);
 
   return (
     <AlignRightDiv>
-      <CSVLink data={csvData} filename="program.csv">
+      <CSVLink headers={csvHeader} data={csvData} filename="program.csv">
         <Button variant="primary">CSV形式でダウンロード</Button>
       </CSVLink>
     </AlignRightDiv>
   );
+}
+
+const BuildCsvHeader = () => (
+  [
+    { label: "部", key: "partNum" },
+    { label: "番", key: "index" },
+    { label: "姓", key: "lastName" },
+    { label: "名", key: "firstName" },
+    { label: "所属", key: "faculty" },
+    { label: "学年", key: "grade" },
+    { label: "作曲者", key: "composer" },
+    { label: "曲名", key: "work" },
+    { label: "演奏時間", key: "playMinutes" },
+    { label: "開始時間", key: "startingTime" },
+    { label: "メモ", key: "memo" }
+  ]
+)
+
+const BuildCsvData = (partMap: PartMap, entryMap: EntryMap) => {
+  
+  const sortedParts: Part[] = Object.values(partMap).sort(
+    (a, b) => a.partNum - b.partNum
+  );
+
+  const DividedData = sortedParts.flatMap((part) => {
+    const entrySchedules = BuildEntrySchedules(part.entryIds, entryMap, part.startingTime);
+    return part.entryIds.map((entryId, index) => {
+      const entry = entryMap[entryId];
+      return DivideEntryForRow({
+        entry,
+        partNum: part.partNum,
+        index: index + 1, // 1-based index
+        stringStartingTime: StringifyDate(entrySchedules[entryId].startingTime),
+      });
+    }).flat();
+  });
+
+  const csvData = DividedData.map((row) => ({
+    partNum: row.partNum,
+    index: row.index,
+    lastName: row.lastName,
+    firstName: row.firstName,
+    faculty: row.faculty,
+    grade: row.grade,
+    composer: row.composer,
+    work: row.work,
+    playMinutes: row.playMinutes || "",
+    startingTime: row.startingTime || "",
+    memo: row.memo || "",
+  }));
+
+  return csvData;
 }
